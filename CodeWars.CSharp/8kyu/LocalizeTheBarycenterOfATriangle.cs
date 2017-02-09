178f49c38871d314be57c0d3ea1403cf5e0e2a91
olezhkalviv <olezhkalviv@gmail.com>
2017-02-09 18:01:14 +0200

    Fixed desc

--- CodeWars.CSharp/8kyu/LocalizeTheBarycenterOfATriangle.cs
+++ CodeWars.CSharp/8kyu/LocalizeTheBarycenterOfATriangle.cs
@@ -2,7 +2,7 @@
 
 namespace CodeWars.CSharp._8kyu.LocalizeTheBarycenterOfATriangle
 {
-    // LocalizeTheBarycenterOfATriangle
+    // Localize The Barycenter of a Triangle
     // https://www.codewars.com/kata/localize-the-barycenter-of-a-triangle/csharp
     public class Barycenter
     {
