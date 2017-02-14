7c133fb92a243b2c2f7846bc7377904516e02fdf
olezhkalviv <olezhkalviv@gmail.com>
2017-02-14 14:53:13 +0200

    Added a new solution

---
+++ CodeWars.CSharp/8kyu/HowOldWillIBeIn2099.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CodeWars.CSharp._8kyu.HowOldWillIBeIn2099
+{
+    // HowOldWillIBeIn2099
+    // https://www.codewars.com/kata/how-old-will-i-be-in-2099
+    public static class AgeDiff
+    {
+        public static string CalculateAge(int birth, int yearTo)
+        {
+            int diff = Math.Abs(yearTo - birth);
+            return yearTo == birth ? "You were born this very year!" : yearTo > birth ? $"You are {diff} year{(diff == 1 ? "" : "s")} old." : $"You will be born in {diff} year{(diff == 1 ? "" : "s")}.";
+        }
+    }
+}
